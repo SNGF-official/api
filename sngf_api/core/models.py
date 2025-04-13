ae69d0eceb7bd0523ae413a76bc6ac1707a35c0e
YlanDecode <rabenaly3@gmail.com>
2025-04-13 15:36:21 +0300

    chore: combine all logic for contact form to one

--- sngf_api/core/models.py
+++ sngf_api/core/models.py
@@ -31,3 +31,20 @@ class Status(models.Model):
 
     def __str__(self):
         return f"{self.id, self.status}"
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+
+    class Meta:
+        abstract = True
+
+
+class Contactable(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    customer_email = models.EmailField(blank=True)
+
+    class Meta:
+        abstract = True
