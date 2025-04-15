b9134a1218794544311a9526b9717bd94a8f049e
YlanDecode <rabenaly3@gmail.com>
2025-04-15 19:33:24 +0300

    Client rest api (#15)
    * feat: migrations
    
    * feat: plant

--- sngf_api/plant/admin.py
+++ sngf_api/plant/admin.py
@@ -2,16 +2,44 @@ from django.contrib import admin
 
 from .models import Plant
 from .models import PlantImage
+from .models import PlantSizePrice
+from .models import Seed
 
 
 class PlantImageInline(admin.TabularInline):
     model = PlantImage
-    extra = 1
+    extra = 1  # Nombre de formulaires vides à afficher pour l'ajout d'images
+
+
+class PlantSizePriceInline(admin.TabularInline):
+    model = PlantSizePrice
+    extra = 1  # Nombre de formulaires vides à afficher pour l'ajout de prix par taille
 
 
 @admin.register(Plant)
 class PlantAdmin(admin.ModelAdmin):
-    inlines = [PlantImageInline]
-    list_display = ("name", "category", "price", "quantity", "status")
-    list_filter = ("category", "size", "status")
+    list_display = ("name", "category", "quantity", "status")
+    list_filter = ("category", "status")
+    search_fields = ("name", "description")
+    inlines = [PlantImageInline, PlantSizePriceInline]
+
+
+@admin.register(PlantImage)
+class PlantImageAdmin(admin.ModelAdmin):
+    list_display = ("plant", "alt_text", "image")
+    list_filter = ("plant",)
+    search_fields = ("alt_text",)
+
+
+@admin.register(PlantSizePrice)
+class PlantSizePriceAdmin(admin.ModelAdmin):
+    list_display = ("plant", "size", "price")
+    list_filter = ("plant", "size")
+    search_fields = ("plant__name",)
+
+
+@admin.register(Seed)
+class SeedAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "quantity", "status", "price_per_kilo")
+    list_filter = ("category", "status")
     search_fields = ("name", "description")
--- sngf_api/plant/models.py
+++ sngf_api/plant/models.py
@@ -7,43 +7,143 @@ from sngf_api.core.models import Status
 
 
 class PlantImage(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    plant = models.ForeignKey("Plant", related_name="images", on_delete=models.CASCADE)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text="Identifiant unique de l'image de la plante."
+        "Il est généré automatiquement et ne peut pas être modifié.",
+    )
+    plant = models.ForeignKey(
+        "Plant",
+        related_name="images",
+        on_delete=models.CASCADE,
+        help_text="La plante à laquelle cette image est associée.",
+    )
     image = models.ImageField(
         upload_to="plants/gallery/",
         validators=[FileExtensionValidator(["jpg", "jpeg", "png", "webp"])],
+        help_text="Le fichier image de la plante."
+        "Les formats supportés sont JPG, JPEG, PNG et WEBP.",
     )
-    alt_text = models.CharField(max_length=255, blank=True)
+    alt_text = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="Texte alternatif pour l'image,"
+        "important pour l'accessibilité et le SEO.",
+    )
 
     def __str__(self):
         return f"Image de {self.plant.name}"
 
 
-class Plant(models.Model):
+class BaseProduct(models.Model):
     class CategoryChoices(models.TextChoices):
-        SEED = "SEED", "Graines"
-        PLANT = "PLANT", "Plante"
+        AGROFORESTIERES = "AGROFORESTIERES", "Agroforestières"
+        ENDEMIQUES_AUTOCHTONES = "ENDEMIQUES_AUTOCHTONES", "Endémiques autochtones"
+        EXOTIQUES_REBOISEMENT = "EXOTIQUES_REBOISEMENT", "Exotiques de reboisement"
+        ORNEMENTALES = "ORNEMENTALES", "Ornementales"
+        EMBROUSSAILLEMENTS = "EMBROUSSAILLEMENTS", "Embrousseillements"
 
-    class SizeChoices(models.TextChoices):
-        XS = "XS", "Très petit"
-        S = "S", "Petit"
-        M = "M", "Moyen"
-        L = "L", "Grand"
-        XL = "XL", "Très grand"
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    category = models.CharField(
-        max_length=10, choices=CategoryChoices.choices, null=False
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text="Identifiant unique du produit de base."
+        "Il est généré automatiquement et ne peut pas être modifié.",
     )
-    type = models.CharField(max_length=100, blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
-    unit = models.CharField(max_length=20, blank=True)
-    quantity = models.PositiveIntegerField()
-    size = models.CharField(max_length=5, choices=SizeChoices.choices, null=False)
-    status = models.CharField(max_length=10, choices=Status.STATUS)
+    name = models.CharField(max_length=100, help_text="Le nom du produit.")
+    category = models.CharField(
+        max_length=30,
+        choices=CategoryChoices.choices,
+        blank=True,
+        help_text="La catégorie du produit."
+        "Vous pouvez choisir parmi les options proposées.",
+    )
+    description = models.TextField(
+        blank=True, help_text="Une description détaillée du produit (facultatif)."
+    )
+    quantity = models.PositiveIntegerField(
+        help_text="La quantité disponible de ce produit."
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=Status.STATUS.choices,
+        help_text="Le statut actuel du produit "
+        "(par exemple, 'Disponible', 'Indisponible').",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class PlantSizePrice(models.Model):
+    class SizeChoices(models.TextChoices):
+        PM = "PM", "Petit modèle"
+        MM = "MM", "Modèle moyen"
+        GM = "GM", "Grand modèle"
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text="Identifiant unique du prix par taille.",
+    )
+    plant = models.ForeignKey(
+        "Plant",
+        related_name="prices",
+        on_delete=models.CASCADE,
+        help_text="La plante à laquelle ce prix et cette taille sont associés.",
+    )
+    size = models.CharField(
+        max_length=2, choices=SizeChoices.choices, help_text="La taille de la plante."
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text="Le prix pour cette taille de plante.",
+    )
+
+    class Meta:
+        unique_together = ("plant", "size")
 
     def __str__(self):
+        return f"{self.plant.name} - {self.get_size_display()}: {self.price}"
+
+
+class Plant(BaseProduct):
+    def __str__(self):
         return self.name
+
+
+class Seed(BaseProduct):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text="Identifiant unique de la graine.",
+    )
+    name = models.CharField(max_length=100, help_text="Le nom des graines.")
+    category = models.CharField(
+        max_length=30,
+        choices=BaseProduct.CategoryChoices.choices,
+        blank=True,
+        help_text="La catégorie des graines.",
+    )
+    description = models.TextField(
+        blank=True, help_text="Une description détaillée des graines (facultatif)."
+    )
+    quantity = models.PositiveIntegerField(
+        help_text="La quantité disponible de ces graines."
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=Status.STATUS.choices,
+        help_text="Le statut actuel des graines.",
+    )
+    price_per_kilo = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="Le prix des graines au kilogramme."
+    )
+
+    def __str__(self):
+        return f"{self.name}"
